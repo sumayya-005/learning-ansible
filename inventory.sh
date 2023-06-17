c88de95b030b8a7931ffc8fda719662680609c28
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-06-17 15:41:32 +0530

    updating ./learning-ansible

--- inventory.sh
+++ inventory.sh
@@ -1,12 +1,12 @@
-172.31.26.28
+172.31.2.185
 
-ansible -i inventory  --help
-
-ansible -i inventory all -u centos -k -m ansible.builtin.ping
-
-command for connecting to the interpreter.
-
- add -b to the commnad to become a rootuser in ansible
-
-
-the solution for AD-HOC is ansible playbook
+#ansible -i inventory  --help
+#
+#ansible -i inventory all -u centos -k -m ansible.builtin.ping
+#
+#command for connecting to the interpreter.
+#
+# add -b to the commnad to become a rootuser in ansible
+#
+#
+#the solution for AD-HOC is ansible playbook
